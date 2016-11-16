a92696133d6e011f9e64fa63006ea5ed186086af
MagDel <pavelrv@yandex.ru>
2016-11-17 00:17:43 +0300

    Подключаем travis

--- trackreader/src/main/java/ru/ogpscenter/tracker/reader/consumer/HttpConsumer.java
+++ trackreader/src/main/java/ru/ogpscenter/tracker/reader/consumer/HttpConsumer.java
@@ -1,5 +1,11 @@
 package ru.ogpscenter.tracker.reader.consumer;
 
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+import org.apache.http.nio.reactor.ConnectingIOReactor;
+import org.apache.http.nio.reactor.IOReactorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +17,14 @@ import java.util.function.Consumer;
 public class HttpConsumer implements Consumer<String> {
     private static final Logger logger = LoggerFactory.getLogger(HttpConsumer.class);
 
-    public HttpConsumer() {
+    public HttpConsumer() throws IOReactorException {
+
+        ConnectingIOReactor ioReactor = new DefaultConnectingIOReactor();
+        PoolingNHttpClientConnectionManager cm =
+                new PoolingNHttpClientConnectionManager(ioReactor);
+        CloseableHttpAsyncClient client =
+                HttpAsyncClients.custom().setConnectionManager(cm).build();
+        client.start();
     }
 
     @Override
