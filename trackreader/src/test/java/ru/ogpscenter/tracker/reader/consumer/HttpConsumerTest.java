ec254e6dc3955ba0ed23b1fcd8504ff0443d8a9e
MagDel <pavelrv@yandex.ru>
2016-11-18 02:27:57 +0300

    fix tests for any env

--- trackreader/src/test/java/ru/ogpscenter/tracker/reader/consumer/HttpConsumerTest.java
+++ trackreader/src/test/java/ru/ogpscenter/tracker/reader/consumer/HttpConsumerTest.java
@@ -15,16 +15,16 @@ import static org.testng.Assert.assertEquals;
 public class HttpConsumerTest {
     @Test
     public void testCreateFullParametersArray() throws Exception {
-        ArrayList<NameValuePair> array = HttpConsumer.createParametersArray(new LocationRecord(1L, "2", "ime", DateTime.parse("2010-06-30T01:20"),
+        ArrayList<NameValuePair> array = HttpConsumer.createParametersArray(new LocationRecord(1L, "2", "ime", DateTime.parse("2010-06-30T01:20Z"),
                 60.55, -120.1, 3.33, 155, true, true));
-        assertEquals(array.toString(), "[eventId=1, deviceId=2, dttm=1277846400000, imei=ime, lat=60.550000, lon=-120.100000, spd=3.3, crs=155]");
+        assertEquals(array.toString(), "[eventId=1, deviceId=2, dttm=1277860800000, imei=ime, lat=60.550000, lon=-120.100000, spd=3.3, crs=155]");
     }
 
     @Test
     public void testCreateMinParametersArray() throws Exception {
-        ArrayList<NameValuePair> array = HttpConsumer.createParametersArray(new LocationRecord(1L, "2", "ime", DateTime.parse("2010-06-30T01:20"),
+        ArrayList<NameValuePair> array = HttpConsumer.createParametersArray(new LocationRecord(1L, "2", "ime", DateTime.parse("2010-06-30T01:20Z"),
                 60.55, -120.1, 3.33, 155, false, false));
-        assertEquals(array.toString(), "[eventId=1, deviceId=2, dttm=1277846400000, imei=ime]");
+        assertEquals(array.toString(), "[eventId=1, deviceId=2, dttm=1277860800000, imei=ime]");
     }
 
 }
